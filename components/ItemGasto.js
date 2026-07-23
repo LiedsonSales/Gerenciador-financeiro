2b6cd95672439e677c2c927fee68cc067ff548db
Liedson Sales <liedson.r.sales@gmail.com>
2026-07-22 23:22:32 -0300

    teste

---
+++ components/ItemGasto.js
@@ -0,0 +1,16 @@
+import { StyleSheet, Text, View,  } from 'react-native';
+
+const ItemGasto = ({descricao, valor, categoria}) => {
+    return (
+        <View>
+            <Text></Text>
+            <Text></Text>
+        </View>
+    )
+}
+
+const styles = StyleSheet.create({
+    linha: {
+        
+    }
+})
