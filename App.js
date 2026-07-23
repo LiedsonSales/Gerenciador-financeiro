0f36c15b2aadee092b3273983554b001f7f3099e
Liedson Sales <liedsonribeiro92@gmail.com>
2026-07-22 22:58:57 -0300

    app

--- App.js
+++ App.js
@@ -1,20 +1,92 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useState } from 'react';
+import { StyleSheet, Text, View, TextInput, Button, FlatList } from 'react-native';
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+  const [gastos, setGastos] = useState([
+    { descricao: 'iFood', valor: 23.50, categoria: 'Alimentação' },
+  ]);
+
+  const [descricaoTexto, setDescricaoTexto] = useState('');
+  const [valorTexto, setValorTexto] = useState('');
+  const [categoriaTexto, setCategoriatexto] = useState('');
+
+  const total = gastos.reduce((soma, item) => soma + item.valor, 0);
+
+  const adicionarGasto = () => {
+    const valorNumero = parseFloat(valorTexto) || 0;
+
+    if (descricaoTexto.trim() === '' || categoriaTexto.trim() === '' || valorNumero <= 0) {
+      return;
+    };
+
+    setGastos([...gastos, {descricao: descricaoTexto, valor: valorNumero, categoria: categoriaTexto}]);
+    setDescricaoTexto('');
+    setValorTexto('');
+    setCategoriatexto('');
+  };
+
+  return(
+    <View style={styles.constainer}>
+
+      <FlatList
+        data={gastos}
+        keyExtractor={(item, index) => index.toString()}
+        renderItem={({item}) => (
+          <Text>{item.descricao} ({item.categoria}) - R$ {item.valor.toFixed(2)}</Text>
+        )}
+        ListHeaderComponent={<Text style={styles.titulo}>Meus Gastos</Text>}
+        ListFooterComponent={<Text style={styles.total}>Total: R$ {total.toFixed(2)}</Text>}
+      />
+
+      <TextInput 
+        style={styles.input}
+        placeholder='Descrição'
+        value={descricaoTexto}
+        onChangeText={setDescricaoTexto}
+      />
+      <TextInput 
+        style={styles.input}
+        placeholder='Valor'
+        value={valorTexto}
+        onChangeText={setValorTexto}
+      />
+      <TextInput 
+        style={styles.input}
+        placeholder='Categoria'
+        value={categoriaTexto}
+        onChangeText={setCategoriatexto}
+      />
+
+      <Button title="Adicionar gasto" onPress={adicionarGasto}/>
+      
     </View>
-  );
+  )
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
+  constainer: {
     backgroundColor: '#fff',
+    flex: 1,
+    paddingTop: 50,
     alignItems: 'center',
-    justifyContent: 'center',
+    padding: 20
+  },  
+  titulo: {
+    fontSize: 24,
+    fontWeight: 'bold',
+    marginBottom: 20
   },
-});
+  input: {
+    borderWidth: 1,
+    borderColor: '#ccc',
+    borderRadius: 8,
+    padding: 10,
+    width: '80%',
+    marginTop: 10
+  },
+  total: {
+    fontSize: 20,
+    fontWeight: 'bold',
+    marginTop: 15
+  }
+})
